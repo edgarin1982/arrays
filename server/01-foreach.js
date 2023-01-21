be1e0f54f85e2732ca012592a1fe2fa2f6b89c3a
edgarin <maed2802@gmail.com>
2023-01-20 22:14:02 -0600

    foreach

---
+++ server/01-foreach.js
@@ -0,0 +1,7 @@
+const letras = ['a', 'b', 'c']
+for (let index = 0; index < letras.length; index++) {
+   const element = letras[index];
+   console.log('For', element);
+    
+}
+letras.forEach(item => console.log(item));
