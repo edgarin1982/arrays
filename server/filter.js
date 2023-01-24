5d7bf5e1242443a550d0d704b6204d1012e9430f
edgarin <maed2802@gmail.com>
2023-01-24 16:50:16 -0600

    actual

--- server/filter.js
+++ server/filter.js
@@ -1,4 +1,4 @@
-const array = ['spray', 'limit', 'elite', 'exuberant']
+/* const array = ['spray', 'limit', 'elite', 'exuberant']
 
 const newArray = [];
 for (let index = 0; index < array.length; index++) {
@@ -14,4 +14,29 @@ console.log('original', array);
 
 const rta = array.filter(item => item.length >= 6);
 console.log('rta', rta);
-console.log('original', array);
+console.log('original', array); */
+
+const orders = [
+    {
+      customerName: "Nicolas",
+      total: 60,
+      delivered: true,
+    },
+    {
+      customerName: "Zulema",
+      total: 120,
+      delivered: false,
+    },
+    {
+      customerName: "Santiago",
+      total: 180,
+      delivered: true,
+    },
+    {
+      customerName: "Valentina",
+      total: 240,
+      delivered: true,
+    },
+  ]
+  const entregados = orders.filter(item => item.delivered && item.total >= 100);
+  console.log('entregados', entregados);
