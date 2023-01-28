c6e74ca60d9077405ca0adfc1cbef3a580e042c6
Edgarin <maed2802@gmail.com>
2023-01-27 19:13:00 -0600

    reduce-data

---
+++ server/reduce-reload.js
@@ -0,0 +1,52 @@
+const items = [1,2,3,2,2,1,3,5,6,7]
+
+const respuesta = items.reduce((objeto, item)=>{
+  if (!objeto[item]) {
+    objeto[item] = 1;    
+  }else{
+    objeto[item] = objeto[item] +1; 
+  }
+  return objeto;
+},{}); 
+console.log(respuesta);
+
+
+const data = [
+    {
+      name: "Nicolas",
+      level: "low",
+    },
+    {
+      name: "Andrea",
+      level: "medium",
+    },
+    {
+      name: "Zulema",
+      level: "hight",
+    },
+    {
+      name: "Santiago",
+      level: "low",
+    },
+    {
+      name: "Valentina",
+      level: "medium",
+    },
+    {
+      name: "Lucia",
+      level: "hight",
+    },
+  ];
+
+  const respuesta2 = data
+
+  .map(item => item.level)
+  .reduce((objeto, item)=>{
+    if (!objeto[item]) {
+      objeto[item] = 1;    
+    }else{
+      objeto[item] = objeto[item] +1; 
+    }
+    return objeto;
+  },{}); 
+  console.log(respuesta2);
