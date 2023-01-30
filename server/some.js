2f913c717641e1330d6b49410a3aa08cdb292fcc
Edgarin <maed2802@gmail.com>
2023-01-30 12:53:52 -0600

    repaso

--- server/some.js
+++ server/some.js
@@ -87,3 +87,21 @@ const orders = [
     })
   }
   console.log('esta sobre puesto', isOverlap(newAppointment));
+
+
+  /* Tu reto es retornar true si dentro de array de números al menos uno es un número, para solucionarlo vas a encontrar una función llamada checkArray que recibe un parámetro de entrada:
+
+    numbers: Un array de números
+
+Dentro del cuerpo de la función checkArray debes escribir tu solución.
+
+Ejemplo 1:
+
+Input: [1, 3, 5, 7, 10, 11]
+Output: true
+
+Ejemplo 2:
+
+Input: [1, 3, 5]
+Output: false
+ */
